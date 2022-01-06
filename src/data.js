52cda5f5bb90443b703380c89042fd112f30cac0
teregonzalez <teregonzalezpro@gmail.com>
2022-01-06 11:59:38 -0300

    Cambios

--- src/data.js
+++ src/data.js
@@ -1,17 +1,9 @@
 
 export const charFilterMovie = (data, nameFilm) => {
-<<<<<<< HEAD
   const charMovie = data.filter (char => char.films == nameFilm);
-=======
-  const charMovie = data.filter (char => char.film == nameFilm);
->>>>>>> 6ff7ea8299a3b741a7524bfde097aeef8b61c7d1
   return charMovie;
 };
 
 export const anotherExample = () => {
   return 'OMG';
 };
-
-
-
-
